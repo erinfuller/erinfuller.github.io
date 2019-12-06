c56d296ce853abe6c324440ab66e81585ecc652d
Erin Fuller <EFULLER@ANDREW.CMU.EDU>
2019-12-06 10:50:31 -0500

    yes

--- daily-horoscope/aries/aries.js
+++
@@ -1,9 +0,0 @@
-//$.ajax({
-//    type:'POST',
-//    url:'https://aztro.sameerkumar.website?sign=aries&day=today',
-//    success:function(data){
-//        console.log(data.description);
-//        aries = data.description;
-////        document.getElementById('horo').innerHTML = data.description;
-//    }
-//});
